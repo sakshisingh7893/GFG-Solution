c32fa64c7ea3da0ab968ddbef0f4cae05c18b85a
sakshisingh7893 <55277983+sakshisingh7893@users.noreply.github.com>
2022-06-27 12:15:40 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -12,11 +12,11 @@ class Solution {
         for(int i=0;i<=m;i++){
             for(int j=0;j<=n;j++){
                 t[i][j] = 0;
-                
+                if(j==0)    t[i][j]=1;
             }
         }
-        for(int i=0; i<=m; i++)
-            t[i][0]=1;
+        //for(int i=0; i<=m; i++)
+          //  t[i][0]=1;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(S[i-1]<=j){
