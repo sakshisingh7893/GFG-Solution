26edafd5cd679e14d4947e1f56bc9ea26a504bae
sakshisingh7893 <55277983+sakshisingh7893@users.noreply.github.com>
2022-06-29 11:47:18 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -9,45 +9,31 @@ class Solution{
 	int minCoins(int coins[], int M, int V) 
 	{ 
 	    // Your code goes here
-	    /*int t[M+1][V+1];
+	    int t[M+1][V+1];
 	    for(int i=0;i<=M;i++){
 	        for(int j=0;j<=V;j++){
-	            t[i][j]=INT_MAX;
+	            t[i][j]=-1;
+	            if(i==0)    t[i][j]=INT_MAX-1;
+	            if(j==0)    t[i][j]=0;
 	        }
 	    }
-	    for(int i=0;i<=V;i++){
-	        t[0][i]=-1;
+	    for(int i=1;i<=V;i++){
+	        if(i%coins[0]==0)
+	            t[1][i] = i/coins[0];
+	       else t[1][i] = INT_MAX-1;
 	    }
-	    for(int i=0;i<=M;i++){
-	        t[i][0]=0;
-	    }*/
-	    int W=V;
-    int n=M;
-    int dp[n+1][W+1];
-    for(int i=0;i<=n;i++)
-     dp[i][0]=0;
-    for(int j=0;j<=W;j++)
-     dp[0][j]=INT_MAX-1;
-    for(int j=1;j<=W;j++){
-     if(j%coins[0]==0)
-      dp[1][j]=j/coins[0];
-     else
-      dp[1][j]=INT_MAX-1;
-    }
-    for(int i=2;i<=n;i++){
-     for(int j=1;j<=W;j++){
-      if(coins[i-1]<=j)
-       dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-      else
-       dp[i][j]=dp[i-1][j];
-     }
-    }
-    if(dp[n][W]==INT_MAX || dp[n][W]==INT_MAX-1)
-        return -1;
-    return dp[n][W];
-	    
-	    
-	}
+	    for(int i=1;i<=M;i++){
+	        for(int j=1;j<=V;j++){
+	            if(coins[i-1]<=j)
+	                t[i][j] = min(t[i][j-coins[i-1]]+1 , t[i-1][j]);
+	            else
+	                t[i][j] = t[i-1][j];
+	        }
+	    }
+	    if(t[M][V]==INT_MAX-1)  return -1;
+	    return t[M][V];
+	} 
+	  
 };
 
 // { Driver Code Starts.
