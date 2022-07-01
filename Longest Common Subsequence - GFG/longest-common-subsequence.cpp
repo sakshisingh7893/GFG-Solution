064337f5dff59b8bd1a61a2b47f8c45cbe7db6ca
sakshisingh7893 <55277983+sakshisingh7893@users.noreply.github.com>
2022-07-01 15:04:27 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -31,6 +31,11 @@ class Solution
             }
         }
         return t[x][y];
+        /*if(x==0 || y==0)    return 0;
+        if(s1[x-1]==s2[y-1])    return 1+lcs(x-1,y-1,s1,s2);
+        else{
+            return max(lcs(x-1,y,s1,s2),lcs(x,y-1,s1,s2));
+        }*/
     }
 };
 
